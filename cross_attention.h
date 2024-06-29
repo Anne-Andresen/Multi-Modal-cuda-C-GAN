e94b11488dd532e596dcedff5dfb82031a1b02f3
Anne-Andresen <56411446+Anne-Andresen@users.noreply.github.com>
2024-06-30 00:09:12 +0200

    finished header file for cross attention

--- cross_attention.h
+++ cross_attention.h
@@ -8,5 +8,22 @@ typedef struct {
     int embed_dim;
     int num_heads;
     int head_dim;
+    float *d_queries;
+    float *d_keys;
+    float *d_values;
+    float *d_output;
+    float *d_Wq;
+    float *d_Wk;
+    float *d_Wv;
+    float *d_Wo;
+    
+    } CrossAttention;
 
-}
+void cross_attention_init(CrossAttention* attn, int embed_dim, int num_heads, int batch_size, int D, int H, int W);
+void cross_attention_set_input(CrossAttention* attn, const float* queries, const float* keys, const float* values);
+void cross_attention_forward(CrossAttention* attn);
+void cross_attention_get_output(CrossAttention* attn, float* output);
+void cross_attention_free(CrossAttention* attn);
+
+
+#endif
