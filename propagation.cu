292527aebbc082238953d793bc248d58179e8a01
Anne-Andresen <56411446+Anne-Andresen@users.noreply.github.com>
2024-07-16 01:37:57 +0200

    backprop

--- propagation.cu
+++ propagation.cu
@@ -15,4 +15,19 @@ void forward(float input[INPUT_SIZE], float weights[INPUT_SIZE][OUTPUT_SIZE], fl
     }
 }
 
-void backward()
+void backward(float input[INPUT_SIZE], float weights[INPUT_SIZE][OUTPUT_SIZE], float output[OUTPUT_SIZE], float target[OUTPUT_SIZE], float dW[INPUT_SIZE][OUTPUT_SIZE]) {
+    float dOutput[OUTPUT_SIZE];
+
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        dOutput[i] = output[i] - target[i]; /*very simple loss for now to be replaced later 111209*/
+        if (output[i] <= 0) dOutput = 0; // derivative of relu
+
+        for (int j = 0; i < INPUT_SIZE; j++) {
+            dW[j][i] = dOutput[i] * input[j];
+
+        }
+    }
+}
+
+
+
