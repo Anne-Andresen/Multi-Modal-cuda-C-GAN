56c63184dd5b7878db5a83383c257f0f9919cc4f
Anne-Andresen <56411446+Anne-Andresen@users.noreply.github.com>
2024-07-23 01:43:07 +0200

    train loop and backprop for ae

--- Makefile
+++ Makefile
@@ -4,12 +4,11 @@ CFLAGS = -I. -g
 DEPS = conv3d.h autoencoder.h loss.h utils.h
 OBJ = main.o conv3d.o autoencoder.o loss.o utils.o
 
-
 %.o: %.cu $(DEPS)
-    $(CC) -c -o $@ $< $(CFLAGS)
+	$(CC) -c -o $@ $< $(CFLAGS)
 
 autoencoder: $(OBJ)
-    $(CC) -o $@ $^ @(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 clean:
-    rm -f *.o autoencoder
+	rm -f *.o autoencoder
--- autoencer.cu
+++ autoencer.cu
@@ -53,6 +53,43 @@ void forward_autoencoder(Autoencoder* autoencoder, float* input, float* output) 
 
 }
 
+void backward_autoencoder(Autoencoder* autoencoder, float* input, float* otuput, float* target, float* learning_rate) {
+    for* grad_output = (float*)safe_malloc(autoencoder->decoder.deconv2.D * autoencoder->decoder.deconv2.H * autoencoder->decoder.deconv2.W * sizeof(float));
+    for (int i = 0; i < autoencoder->decoder.deconv2.D * autoencoder->decoder.deconv2.H * autoencoder->decoder.deconv2.W; i ++) {
+        grad_output[i] = 2.0 * (output[i] - target[i]);
+    }
+    float* grad_latent_space = (float*)safe_malloc(autoencoder->decoder.deconv1.D * autoencoder.decoder.deconv1.H * autoencoder->decoder.deconv1.W *sizeof(float));
+    conv3d_backprop(&autoencoder->decoder.deconv2, grad_output, grad_latent_space);
+    float* grad_intermediate = (float*)safe_malloc(autoencoder->encoder.conv2.D * autoencoder.encoder.conv2.H * autoencoder->encoder.conv2.W * sizeof(float));
+    conv3d_backprop(&autoencoder->decoder.deconv1, grad_latent_space, grad_biases);
+    float* grad_input = (float*)safe_malloc(autoencoder->encoder.conv1.D * autoencoder->encoder.conv1.H * autoencoder->encoder.W * sizeof(float));
+    conv3d_backprop(&autoencoder->encoder.conv2, grad_intermediate, grad_input);
+
+    conv3d_backprop(&autoencoder->encoder.conv1, grad_input, NULL);
+
+    conv3d_update_weights(&autoencoder->encoder.conv1, learning_rate);
+    conv3d_update_weights(&autoencoder->encoder.conv2, learning_rate);
+    conv3d_update_weights(&autoencoder->decoder.deconv1, learning_rate);
+    conv3d_update_weights(&autoencoder->decoder.deconv2, learning_rate);
+    
+    free(grad_output);
+    free(grad_latent_space);
+    free(grad_intermediate);
+    free(grad_input);
+
+}
+
+void train_autoencoder(Autoencoder* autoencoder, float* input, float* target, int inputSize, int epochs, float learning_rate) {
+    float* output = (float*)safe_malloc(inputSize *  sizeof(float));
+    for (int epoch = 0; epoch < epochs; epoch++) {
+        forward_autoencoder(autoencoder, input, output);
+        float loss = mean_square_error(output, target, inputSize);
+        fprint("Epoch %d, Loss: %f\n", epoch, loss);
+        backward_autoencoder(autoencoder, input, output, target, learning_rate);
+    }
+    free(output);
+}
+
 void free_encoder(Encoder* encoder) {
     conv3d_free(&encoder->conv1);
     conv3d_free(&encoder->conv2);
